6d16181a2d37b0df9c1646ac5316eb8a35e45245
Ashton996 <aramseur9620@student.clevelandcountyschools.org>
2024-12-06 09:31:49 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function mouseHover() {
+  document.getElementById("OriginalImage").src = "https://res.cloudinary.com/dbuh4wfsw/image/upload/v1733495199/cat_yfmqxq_c_fill_w_960_h_540_vohfcl.jpg";
+}
+function mouseOut(){
+ document.getElementById("OriginalImage").src = "https://res.cloudinary.com/dbuh4wfsw/image/upload/v1730465334/strawberry-food_PROXEBKRSP_lorcb2.jpg";}
